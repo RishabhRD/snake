85933961bdf29d0ddd1a93cf24acc084ba81ba02
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2022-01-26 21:30:21 +0530

    bye bye pixel_converter_t

--- src/main.cpp
+++ src/main.cpp
@@ -13,22 +13,10 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window/WindowStyle.hpp>
 
-
-class pixel_converter_t {
-  int scaling_factor_x_;
-  int scaling_factor_y_;
-
-public:
-  pixel_converter_t(int scaling_factor_x, int scaling_factor_y)
-    : scaling_factor_x_(scaling_factor_x), scaling_factor_y_(scaling_factor_y) {
-  }
-
-  [[nodiscard]] auto operator()(snk::point_t point) const -> sf::Vector2f {
-    return { static_cast<float>(point.x * scaling_factor_x_),
-      static_cast<float>(point.y * scaling_factor_y_) };
-  }
-};
-
+auto pixel_on_draw_board(snk::point_t point) -> sf::Vector2f {
+  return { static_cast<float>(point.x * snk::game_data::scaling_factor_x),
+    static_cast<float>(point.y * snk::game_data::scaling_factor_y) };
+}
 
 auto poll_events(sf::RenderWindow &window, snk::state_t &game_state) {
   sf::Event event{};
@@ -40,58 +28,42 @@ auto poll_events(sf::RenderWindow &window, snk::state_t &game_state) {
 
 void draw_tile(snk::point_t const position,
   sf::Color const &color,
-  sf::RenderWindow &window,
-  pixel_converter_t const to_pixel) {
-  sf::RectangleShape rect{ to_pixel({ 1, 1 }) };
+  sf::RenderWindow &window) {
+  sf::RectangleShape rect{ pixel_on_draw_board({ 1, 1 }) };
   rect.setFillColor(color);
-  rect.setPosition(to_pixel(position));
+  rect.setPosition(pixel_on_draw_board(position));
   window.draw(rect);
 }
 
-void draw_snake_head(sf::RenderWindow &window,
-  snk::running_t const &state,
-  pixel_converter_t const to_pixel) {
-  draw_tile(state.snake().head(), sf::Color::Blue, window, to_pixel);
+void draw_snake_head(sf::RenderWindow &window, snk::running_t const &state) {
+  draw_tile(state.snake().head(), sf::Color::Blue, window);
 }
 
-void draw_snake_body(sf::RenderWindow &window,
-  snk::running_t const &state,
-  pixel_converter_t const to_pixel) {
+void draw_snake_body(sf::RenderWindow &window, snk::running_t const &state) {
   std::for_each(begin(state.snake().body_points()),
     std::prev(end(state.snake().body_points())),
-    [&](auto body_point) {
-      draw_tile(body_point, sf::Color::Green, window, to_pixel);
-    });
+    [&](auto body_point) { draw_tile(body_point, sf::Color::Green, window); });
 }
 
-void draw_snake(sf::RenderWindow &window,
-  snk::running_t const &state,
-  pixel_converter_t const to_pixel) {
-  draw_snake_head(window, state, to_pixel);
-  draw_snake_body(window, state, to_pixel);
+void draw_snake(sf::RenderWindow &window, snk::running_t const &state) {
+  draw_snake_head(window, state);
+  draw_snake_body(window, state);
 }
 
-void draw_fruit(sf::RenderWindow &window,
-  snk::running_t const &state,
-  pixel_converter_t const to_pixel) {
-  draw_tile(state.fruit_pos(), sf::Color::Red, window, to_pixel);
+void draw_fruit(sf::RenderWindow &window, snk::running_t const &state) {
+  draw_tile(state.fruit_pos(), sf::Color::Red, window);
 }
 
-void draw(sf::RenderWindow &window,
-  snk::running_t const &state,
-  pixel_converter_t const to_pixel) {
+void draw(sf::RenderWindow &window, snk::running_t const &state) {
   window.clear(sf::Color::Black);
-  draw_fruit(window, state, to_pixel);
-  draw_snake(window, state, to_pixel);
+  draw_fruit(window, state);
+  draw_snake(window, state);
 }
 
-void draw(sf::RenderWindow &window,
-  snk::state_t const &game_state,
-  pixel_converter_t const to_pixel) {
-  auto draw_states = rd::overload{ [&](snk::running_t const &state) {
-                                    draw(window, state, to_pixel);
-                                  },
-    [](auto const &) {} };
+void draw(sf::RenderWindow &window, snk::state_t const &game_state) {
+  auto draw_states =
+    rd::overload{ [&](snk::running_t const &state) { draw(window, state); },
+      [](auto const &) {} };
   std::visit(std::move(draw_states), game_state);
 }
 
@@ -110,10 +82,7 @@ auto main() -> int {
       rd::then<snk::running_t>(state, snk::move_snake);
       if (rd::is<snk::running_t>(state)) snk::check_collision(state);
     });
-    draw(window,
-      game_state,
-      pixel_converter_t{
-        snk::game_data::scaling_factor_x, snk::game_data::scaling_factor_y });
+    draw(window, game_state);
     window.display();
     using namespace std::literals;
     std::this_thread::sleep_for(50ms);
