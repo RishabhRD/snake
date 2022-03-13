97dfcbe43bc3a071c101eb3d54c82ae5b8c10f19
Pascal Menuet <menuet@users.noreply.github.com>
2022-03-13 20:24:33 +0100

    Partial fixes for building with VS2022 (this will not fix boost-ext-ut issue with modules; it needs version 1.1.9)

--- test/test_include.hpp
+++ test/test_include.hpp
@@ -1,3 +1,15 @@
+#pragma once
+
+#if defined(_MSC_VER)
+#define BOOST_UT_DISABLE_MODULE
+#pragma warning(push)
+#pragma warning(disable : 4459)
+#endif
+
 #include <boost/ut.hpp>
+
+#if defined(_MSC_VER)
+#pragma warning(pop)
+#endif
 
 using namespace boost::ut;
