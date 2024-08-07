f70e1921edd9d91581b54e2b8d0a8be680a7088c
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-08-08 00:51:37 +0530

    board definition added

--- include/board.hpp
+++ include/board.hpp
@@ -1,20 +1,19 @@
 #pragma once
 
+#include "point.hpp"
+#include "snake.hpp"
+#include "mod_int.hpp"
 #include <cstddef>
-#include <compare>
 namespace snk {
-class board_t {
-  int width_{};
-  int height_{};
-
-public:
-  explicit board_t(int width, int height)// NOLINT
-    : width_(width), height_(height) {}
-
-  [[nodiscard]] auto width() const -> int { return width_; }
-
-  [[nodiscard]] auto height() const -> int { return height_; }
-
-  friend auto operator==(const board_t &, const board_t &) -> bool = default;
+// TODO: I couldn't find any invariants this class can hold in a convincing
+// way. There is surely some constraints like fruit should be inside board
+// dimensions only. However, don't know how to express them.
+// Feel free to raise a PR if you can figure it out or an issue if you can
+// justify the current decision :)
+struct board {
+  std::size_t height;
+  std::size_t width;
+  snake<mod_int> snake;
+  point<mod_int> fruit_pos;
 };
 }// namespace snk
