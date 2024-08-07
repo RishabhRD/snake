7ccb5ec9d2ed6f0bfb4f5d5b5aa50afe442aa1d8
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-08-08 03:48:31 +0530

    added test for matrix_algorithm

---
+++ tests/matrix_algorithm_test.cpp
@@ -0,0 +1,21 @@
+#include "doctest.hpp"
+#include "matrix_algorithm.hpp"
+
+test("count") {
+  std::vector<std::vector<int>> matrix{ { 0, 1, 0 }, { 0, 0, 0 } };
+  req(nostd::count(matrix, 0) == 5);
+}
+
+test("find_n") {
+  std::vector<std::vector<int>> matrix{ { 0, 1, 0 }, { 0, 0, 0 } };
+  auto [i, j] = nostd::find_n(matrix, 0, 3);
+  req(i == 1);
+  req(j == 0);
+}
+
+test("find") {
+  std::vector<std::vector<int>> matrix{ { 0, 1, 0 }, { 0, 0, 0 } };
+  auto [i, j] = nostd::find(matrix, 0);
+  req(i == 0);
+  req(j == 0);
+}
