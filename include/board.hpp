a4077f5118b91c7de7739c1dec511a338750dce2
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-08-08 01:04:28 +0530

    correct grammar

--- include/board.hpp
+++ include/board.hpp
@@ -7,7 +7,7 @@
 namespace snk {
 // TODO: I couldn't find any invariants this class can hold in a convincing
 // way. There is surely some constraints like fruit should be inside board
-// dimensions only. However, don't know how to express them.
+// dimensions only. However, I don't know how to express them.
 // Feel free to raise a PR if you can figure it out or an issue if you can
 // justify the current decision :)
 struct board {
