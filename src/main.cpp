7126eac20cb6df8e9224ad9f12327a1b98185559
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-08-14 02:17:57 +0530

    initial main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,11 @@
-int main() {}
+#include <chrono>
+#include "init.hpp"
+#include "random.hpp"
+#include "run.hpp"
+
+int main(int argc, char** argv) {
+  auto prop = snk::init_config(argc, argv);
+  auto seed = std::chrono::steady_clock::now().time_since_epoch().count();
+  snk::random_gen gen(static_cast<unsigned long>(seed));
+  snk::run(std::move(prop), gen);
+}
