598b30c032d8e31c51d3ee89291c48ccd5962efc
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-08-14 00:31:09 +0530

    rendering template added

---
+++ include/render.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "state.hpp"
+#include "ui/window.hpp"
+
+namespace snk {
+template <ui::Window window_t>
+void render(window_t&, states::state const&) {
+  // TODO: implement rendering
+}
+}  // namespace snk
--- include/ui/window.hpp
+++ include/ui/window.hpp
@@ -5,7 +5,7 @@
 
 namespace snk::ui {
 template <typename T>
-concept UIWindow = requires(T& win, T const& c_win) {
+concept Window = requires(T& win, T const& c_win) {
   { width(c_win) } -> std::same_as<std::size_t>;
   { height(c_win) } -> std::same_as<std::size_t>;
   { poll_events(win) } -> std::same_as<std::vector<events::event>>;
