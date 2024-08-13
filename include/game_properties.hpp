5a9641ed69136f3c424332c42122416dff568407
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-08-13 21:24:58 +0530

    game properties should have speed

--- include/game_properties.hpp
+++ include/game_properties.hpp
@@ -7,6 +7,7 @@ namespace snk {
 struct game_properties {
   std::size_t board_width;
   std::size_t board_height;
+  std::size_t init_speed;
   point<std::size_t> init_fruit_pos;
   point<std::size_t> init_snake_head_pos;
 };
