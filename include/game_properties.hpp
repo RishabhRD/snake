2da10be5ecc118bfb7167d19d25b005fc53ef26e
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2024-08-13 21:39:29 +0530

    game properties should directly contain snake

--- include/game_properties.hpp
+++ include/game_properties.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <cstddef>
+#include "mod_int.hpp"
 #include "point.hpp"
+#include "snake.hpp"
 
 namespace snk {
 struct game_properties {
@@ -9,6 +11,6 @@ struct game_properties {
   std::size_t board_height;
   std::size_t init_speed;
   point<std::size_t> init_fruit_pos;
-  point<std::size_t> init_snake_head_pos;
+  snk::snake<mod_int> init_snake;
 };
 }  // namespace snk
